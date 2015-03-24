e0719f067fc23975c3c54be44de3ea4a90d94833
Jin Han Park <wakeup5@outlook.kr>
2015-03-24 00:42:54 +0000

    Imported from Changeset 14

---
+++ 03-24 Lesson - 1/b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "µÎ¹øÂ°" << endl;
+
+	return 0;
+}
---
+++ 03-24 Lesson - 2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 50;
+	int b = 30;
+	
+	a -= b;
+
+	cout << "a - b의 값은 " << a << " 입니다." << endl;
+	
+	return 0;
+}
