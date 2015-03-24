9ffa271d3a46fd3194920f2e104dc2a25e32fb94
Jin Han Park <wakeup5@outlook.kr>
2015-03-24 01:01:09 +0000

    Imported from Changeset 15

--- 03-24 Lesson - 2/main.cpp
+++ 03-24 Lesson - 2/main.cpp
@@ -4,12 +4,34 @@ using namespace std;
 
 int main()
 {
+	cout << boolalpha;
+	/*
 	int a = 50;
 	int b = 30;
 	
 	a -= b;
 
 	cout << "a - b의 값은 " << a << " 입니다." << endl;
-	
+	*/
+
+	/*
+	int a = 0;
+
+	cout << "a의 값은 " << a << ", a++의 값은 " << a++ << endl;
+	*/
+
+	/*
+	bool result;
+
+	int num1 = 0;
+	int num2 = 100;
+
+	result = num1 < num2;
+
+	cout << "결과는 : " << result << endl;
+	*/
+
+
+
 	return 0;
 }
