0bf44f0adc3c1ff1d0f40581e7c534366f539168
Jin Han Park <wakeup5@outlook.kr>
2015-03-24 00:19:52 +0000

    Imported from Changeset 13

---
+++ Class(03-24)/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age = 0;
+	float height = 0;
+
+	age = 25;
+	height = 165.5;
+
+	cout << "나이는 " << age << "살" << endl;
+	cout << "키는 " << height << "Cm" << endl;
+
+	return 0;
+}
