36d56532aeb7552efcf4d7077e53a673d98fbda0
Jin Han Park <wakeup5@outlook.kr>
2015-03-31 01:13:38 +0000

    Imported from Changeset 54

---
+++ 03-31 Lesson - 1 포인터/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//포인터 
+	/*
+	int a = 10;
+	int* b;
+
+	cout << &a;
+	*/
+
+	//*는 가리키는것. &는 값을 가리키는 주소값.
+
+	//*b = a;
+
+	//cout << b;
+
+	char* aa = "abcde";
+	char* bb = ("adcbe");
+
+	cout << strcmp(aa, bb);
+
+	cout << &"abcde";
+
+	const char *a = "야호";
+	int num = strlen(a);
+
+	return 0;
+}
