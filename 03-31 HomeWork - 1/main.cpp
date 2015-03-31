fd01f0ff803784cfc9ab9e25ee0d2821d21b54f8
Jin Han Park <wakeup5@outlook.kr>
2015-03-31 03:44:54 +0000

    Imported from Changeset 56

--- 03-31 HomeWork - 1/main.cpp
+++ 03-31 HomeWork - 1/main.cpp
@@ -11,3 +11,51 @@
 //i
 //love
 //you
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[100];
+	char str2[100];
+	char* str3 = NULL;
+	
+	cout << "1, 2번 문제 입력 > ";
+	cin.getline(str, 100);
+	cin.ignore();
+
+	cout << "문제 1 - 문자열 거꾸로 출력." << endl;
+	for (int i = strlen(str) - 1; i >= 0; i--)
+	{
+		cout << str[i];
+	}
+
+	cout << endl << "문제 2 - 문자열 짝수자리만 거꾸로 출력." << endl;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << str[i];
+		}
+		else
+		{
+			cout << str[strlen(str) - i - (strlen(str) % 2 == 0 ? 0 : 1)];
+		}
+	}
+	
+	cout << endl << "3번 문제입력. 띄어쓰기가 포함된 문장 입력. >";
+	
+	cin.getline(str2, 100);
+
+	str3 = strtok(str2, " ");
+	cout << str3 << endl;
+
+	while (str3 = strtok(NULL, " "))
+	{
+		cout << str3 << endl;
+	}
+
+	return 0;
+}
