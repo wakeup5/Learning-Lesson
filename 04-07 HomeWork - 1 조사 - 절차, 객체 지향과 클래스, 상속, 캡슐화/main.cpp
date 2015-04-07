d2b0c65974c6860393466878255cc82b395056f6
Jin Han Park <wakeup5@outlook.kr>
2015-04-07 02:05:17 +0000

    Imported from Changeset 71

--- 04-07 HomeWork - 1 조사 - 절차, 객체 지향과 클래스, 상속, 캡슐화/main.cpp
+++ 04-07 HomeWork - 1 조사 - 절차, 객체 지향과 클래스, 상속, 캡슐화/main.cpp
@@ -16,19 +16,27 @@
 #include <iostream>
 #include <conio.h>
 #include <time.h>
+#include <Windows.h>
 
 using namespace std;
 
 enum INPUT_ARROW
 {
-	INPUT_ARROW_UP, INPUT_ARROW_DOWN, INPUT_ARROW_LEFT, INPUT_ARROW_RIGHT
+	INPUT_ARROW_NONE, INPUT_ARROW_UP, INPUT_ARROW_DOWN, INPUT_ARROW_LEFT, INPUT_ARROW_RIGHT
 };
 
 INPUT_ARROW getArrow();
 void createBoardAndSuffle();
 void print();
+void findZero();
+void positionChange(int, int, int, int);
+void moveUp();
+void moveDown();
+void moveLeft();
+void moveRight();
 
 int board[5][5];
+int x = 0, y = 0;
 
 void main()
 {
@@ -36,29 +44,32 @@ void main()
 	createBoardAndSuffle();
 	while (true)
 	{
+		findZero();
+		print();
 		switch (getArrow())
 		{
 		case INPUT_ARROW_UP:
-			
+			moveUp();
 			break;
 		case INPUT_ARROW_DOWN:
-
+			moveDown();
 			break;
 		case INPUT_ARROW_LEFT:
-
+			moveLeft();
 			break;
 		case INPUT_ARROW_RIGHT:
-
+			moveRight();
 			break;
+		default:
+			continue;
 		}
-		print();
 	}
 }
 
 INPUT_ARROW getArrow()
 {
 	int input;
-	INPUT_ARROW result = INPUT_ARROW_UP;
+	INPUT_ARROW result = INPUT_ARROW_NONE;
 	input = _getch();
 
 	switch (input)
@@ -75,7 +86,11 @@ INPUT_ARROW getArrow()
 	case 80:
 		result = INPUT_ARROW_DOWN;
 		break;
+	case 224:
+	default :
+		result = INPUT_ARROW_NONE;
 	}
+	printf("input : %d\n", input);
 
 	return result;
 }
@@ -88,7 +103,7 @@ void createBoardAndSuffle()
 	{
 		board[i / 5][i % 5] = 24 - i;
 	}
-		
+	/*
 	for (int i = 0; i < 1000; i++)
 	{
 		dest = (rand() % 24);
@@ -98,6 +113,7 @@ void createBoardAndSuffle()
 		board[dest / 5][dest % 5] = board[sour / 5][sour % 5];
 		board[sour / 5][sour % 5] = temp;
 	}	
+	*/
 }
 
 void print()
@@ -111,7 +127,67 @@ void print()
 		}
 		printf("\n");
 	}
+	printf("\n\ny : %d, x : %d\n", y, x);
 }
+
+void findZero(void)
+{
+	for (int i = 0; i < 25; i++)
+	{
+		if (board[i / 5][i % 5] == 0)
+		{
+			x = i % 5;
+			y = i / 5;
+			return;
+		}
+	}
+}
+
+void positionChange(int n3, int n4)
+{
+	int temp;
+
+	temp = board[y][x];
+	board[y][x] = board[n3][n4];
+	board[n3][n4] = temp;
+
+	x = n4;
+	y = n3;
+}
+
+void moveUp()
+{
+	if (y <= 0) return;
+
+	findZero();
+	positionChange(y--, x);
+}
+
+void moveDown()
+{
+	if (y >= 4) return;
+
+	findZero();
+	positionChange(y++, x);
+}
+
+void moveLeft()
+{
+	if (x <= 0) return;
+
+	findZero();
+	positionChange(y, x--);
+}
+
+void moveRight()
+{
+	if (x >= 4) return;
+
+	findZero();
+	positionChange(y, x++);
+}
+
+
 
 //헤더파일 : 머리라고 생각하면 된다. 클래스 혹은 함수등을 알 수 있도록 원형을 작성한다.
 //cpp(소스)파일 : 
